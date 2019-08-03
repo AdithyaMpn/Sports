8208862c5671f3dee9fef5f13eced5e0a4a2d57e
Adithya M P N <mpn.adithya@gmail.com>
2019-08-03 23:12:40 +0530

    Ask the user about goal shot position

---
+++ app/src/main/java/com/adithya/sports/GoalEvent.java
@@ -0,0 +1,15 @@
+package com.adithya.sports;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class GoalEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_event);
+
+    }
+}
--- app/src/main/java/com/adithya/sports/MainActivity.java
+++ app/src/main/java/com/adithya/sports/MainActivity.java
@@ -1,7 +1,10 @@
 package com.adithya.sports;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goalEvent(View view){
+        Intent intent = new Intent(this, GoalEvent.class);
+        startActivity(intent);
+        Toast.makeText(this,"Touch the Field Where the GOAL is Shot from", Toast.LENGTH_LONG).show();
     }
 }
