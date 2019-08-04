bdd4bffb688a11994b6bea70d6e250767be7b52f
Adithya M P N <mpn.adithya@gmail.com>
2019-08-04 22:13:04 +0530

    Get X and Y Coordinates In ImageView

--- app/src/main/java/com/adithya/sports/GoalEvent.java
+++ app/src/main/java/com/adithya/sports/GoalEvent.java
@@ -1,15 +1,43 @@
 package com.adithya.sports;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class GoalEvent extends AppCompatActivity {
 
+
+
+    int x,y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal_event);
 
+        Log.d("GoalEvent", "Hello World");
+
+       final ImageView cp = findViewById(R.id.imageView);
+
+        cp.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                x = (int) event.getX();
+                y = (int) event.getY();
+
+                Log.v("x val of cap img >>", x + "");
+                Log.v("y val of cap img >>", y + "");
+
+
+                return false;
+            }
+        });
     }
+
+
+
+
+
 }
