60a8ee260316f366caf6ccb709e466e48bf81942
Adithya M P N <mpn.adithya@gmail.com>
2019-08-05 21:53:49 +0530

    Draw a Blue Circle on Football field when user touches

--- app/src/main/java/com/adithya/sports/GoalEvent.java
+++ app/src/main/java/com/adithya/sports/GoalEvent.java
@@ -1,5 +1,9 @@
 package com.adithya.sports;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -20,7 +24,7 @@ public class GoalEvent extends AppCompatActivity {
         Log.d("GoalEvent", "Hello World");
 
        final ImageView cp = findViewById(R.id.imageView);
-
+       //final Bitmap bitmap = ((BitmapDrawable)cp.getDrawable()).getBitmap().copy(Bitmap.Config.ARGB_8888, true);
         cp.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -31,6 +35,17 @@ public class GoalEvent extends AppCompatActivity {
                 Log.v("y val of cap img >>", y + "");
 
 
+                Bitmap bitmap = Bitmap.createBitmap(400,300, Bitmap.Config.ARGB_8888);
+                bitmap = bitmap.copy(bitmap.getConfig(), true);
+                Canvas canvas = new Canvas(bitmap);
+                Paint paint = new Paint();
+                paint.setColor(Color.BLUE);
+                paint.setStyle(Paint.Style.FILL);
+                paint.setAntiAlias(true);
+                cp.setImageBitmap(bitmap);
+                cp.setBackgroundResource(R.drawable.footballfield);
+                canvas.drawCircle(x/3,y/3, 5, paint);
+                cp.invalidate();
                 return false;
             }
         });
