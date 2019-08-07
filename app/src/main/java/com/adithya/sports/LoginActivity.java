2bf1377983e4ccfe4a8f2fb6e0f83cbe2bd327e5
Adithya M P N <mpn.adithya@gmail.com>
2019-08-07 13:47:51 +0530

    Debug - Back Button Press

--- app/src/main/java/com/adithya/sports/LoginActivity.java
+++ app/src/main/java/com/adithya/sports/LoginActivity.java
@@ -61,6 +61,8 @@ public class LoginActivity extends AppCompatActivity {
 
                 // ...
             } else {
+
+                finishAffinity();
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
                 // response.getError().getErrorCode() and handle the error.
